e3b73510518acf1c4a0c2d8875b613d6e2755b9c
ChristopherS24 <chryskies@gmail.com>
2024-09-10 01:56:01 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  data() {
+    return {
+        message: 'Template Vite + Vue'
+    }
+  }
+}
+</script>
+
+<template>
+    <div>
+        <h1>
+            {{  message  }}
+        </h1>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
